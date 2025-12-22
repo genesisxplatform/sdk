6cb8092a8af474ca8cb4dc7523e2d8246c90e90d
sergokiko <sergokiko@gmail.com>
2025-12-22 16:17:36 +0100

    fix svg

--- src/sdk-nextjs/components/Preview/ChevronIcon.tsx
+++ src/sdk-nextjs/components/Preview/ChevronIcon.tsx
@@ -10,7 +10,7 @@ export const ChevronIcon = ({ className }: { className?: string }) => {
                   <use fill="#000000" xlinkHref="#path-1"></use>
                   <use fill="#64686A" xlinkHref="#path-1"></use>
               </g>
-              <polyline id="Path" stroke="#FFFFFF" stroke-width="2" points="20 16.5 25 21.5 20 26.5"></polyline>
+              <polyline id="Path" stroke="#FFFFFF" strokeWidth="2" points="20 16.5 25 21.5 20 26.5"></polyline>
           </g>
       </g>
   </svg>
